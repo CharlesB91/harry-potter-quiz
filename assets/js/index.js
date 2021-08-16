1fe2f96f044203a315d8cd188f9f1e2ca14122d9
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-08-16 22:19:24 +0100

    javascript added for instructions functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,9 @@
+// Instructions Bar //
 
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
