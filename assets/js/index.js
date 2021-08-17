4b4bdb7909ab65972d1086f944c4b304c589f05f
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-08-17 13:25:12 +0100

    correct answers added to js object

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,48 @@
+// questions ///
+
+const questions = [
+  {
+    question: "What Is Hermione's Cat Called",
+    answer1: "Scabbers",
+    answer2: "Hedwig",
+    answer3: "Crookshanks",
+    answer4: "Snape",
+    correct: answer3,
+  },
+  {
+    question: "What Is Voldemort's Real Name",
+    answer1: "Tom Riddle",
+    answer2: "Albus Dumbledore",
+    answer3: "Bellatrix Lestrange",
+    answer4: "Ron Weasley",
+    correct: answer1,
+  },
+  {
+    question: "What Was The Last Horcrux",
+    answer1: "Harry Potter",
+    answer2: "Nagini",
+    answer3: "The Cup",
+    answer4: "Tom Riddle's Diary",
+    correct: answer2,
+  },
+  {
+    question: "Who Killed Dumbledore",
+    answer1: "Voldermort",
+    answer2: "Hagrid",
+    answer3: "Severus Snape",
+    answer4: "Draco Malfoy",
+    correct: answer3,
+  },
+  {
+    question: "What Was Harry Potter's Mums Name",
+    answer1: "Lilly",
+    answer2: "Margaret",
+    answer3: "Karen",
+    answer4: "Sandra",
+    correct: answer1,
+  },
+];
+
 let startButton = document.getElementById("start-btn");
 startButton.addEventListener("click", startGame);
 
@@ -16,44 +61,6 @@ function CheckAnswers() {}
 function CheckScore() {}
 
 // questions
-
-const questions = [
-  {
-    question: "What Is Hermione's Cat Called",
-    answer1: "Scabbers",
-    answer2: "Hedwig",
-    answer3: "Crookshanks",
-    answer4: "Snape",
-  },
-  {
-    question: "What Is Voldemort's Real Name",
-    answer1: "Tom Riddle",
-    answer2: "Albus Dumbledore",
-    answer3: "Bellatrix Lestrange",
-    answer4: "Ron Weasley",
-  },
-  {
-    question: "What Was The Last Horcrux",
-    answer1: "Harry Potter",
-    answer2: "Nagini",
-    answer3: "The Cup",
-    answer4: "Tom Riddle's Diary",
-  },
-  {
-    question: "Who Killed Dumbledore",
-    answer1: "Voldermort",
-    answer2: "Hagrid",
-    answer3: "Severus Snape",
-    answer4: "Draco Malfoy",
-  },
-  {
-    question: "What Was Harry Potter's Mums Name",
-    answer1: "Lilly",
-    answer2: "Margaret",
-    answer3: "Karen",
-    answer4: "Sandra",
-  },
-];
 
 // Instructions Bar //
 
