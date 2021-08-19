ef50ac8086095c1351db13c9106ec44a69183550
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-08-19 16:34:49 +0100

    game over function added instead in end js file

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,16 +1,20 @@
 let mostRecentScore = localStorage.getItem("most-recent-score");
 let scoreAlert = document.getElementById("sorting-hat-greeting");
 let houseScore = document.getElementById("house-sort");
-scoreAlert.innerText = `You Scored ${mostRecentScore} out of 5`;
+scoresOnDoors();
 
-if (mostRecentScore == 5) {
-  houseScore.innerText = "Better Be gryffindor !!!";
-} else if (mostRecentScore < 5 && mostRecentScore > 3) {
-  houseScore.innerText = "Welcome To Hufflepuff !!!";
-} else if (mostRecentScore < 4 && mostRecentScore > 2) {
-  houseScore.innerText = "Welcome To ravenclaw !!!";
-} else if (mostRecentScore < 3 && mostRecentScore > 1) {
-  houseScore.innerText = "Welcome To ravenclaw !!!";
-} else {
-  houseScore.innerText = "Down to the dungeons in slytherin";
+function scoresOnDoors() {
+  scoreAlert.innerText = `You Scored ${mostRecentScore} out of 5`;
+
+  if (mostRecentScore == 5) {
+    houseScore.innerText = "Better Be gryffindor !!!";
+  } else if (mostRecentScore < 5 && mostRecentScore > 3) {
+    houseScore.innerText = "Welcome To Hufflepuff !!!";
+  } else if (mostRecentScore < 4 && mostRecentScore > 2) {
+    houseScore.innerText = "Welcome To ravenclaw !!!";
+  } else if (mostRecentScore < 3 && mostRecentScore > 1) {
+    houseScore.innerText = "Welcome To ravenclaw !!!";
+  } else {
+    houseScore.innerText = "Down to the dungeons in slytherin";
+  }
 }
