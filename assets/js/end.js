f753fb20179707a9cf1dc167f82f98877aead299
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-08-23 14:41:42 +0100

    house badges added to game over page

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,19 +1,25 @@
+// Global DOM Selectors //
+
 let mostRecentScore = localStorage.getItem("most-recent-score");
 let scoreAlert = document.getElementById("sorting-hat-greeting");
 let houseScore = document.getElementById("house-sort");
 scoresOnDoors();
 
+// Sorting Function. Depending on
+// score player will be sorted into the house
+// according to their score
+
 function scoresOnDoors() {
   scoreAlert.innerText = `You Scored ${mostRecentScore} out of 5`;
 
   if (mostRecentScore == 5) {
-    houseScore.innerText = "Better Be gryffindor !!!";
+    houseScore.innerText = "Better Be gryffindor";
   } else if (mostRecentScore < 5 && mostRecentScore > 3) {
-    houseScore.innerText = "Welcome To Hufflepuff !!!";
+    houseScore.innerText = "Welcome To Hufflepuff";
   } else if (mostRecentScore < 4 && mostRecentScore > 2) {
-    houseScore.innerText = "Welcome To ravenclaw !!!";
+    houseScore.innerText = "Welcome To ravenclaw";
   } else if (mostRecentScore < 3 && mostRecentScore > 1) {
-    houseScore.innerText = "Welcome To ravenclaw !!!";
+    houseScore.innerText = "Welcome To ravenclaw";
   } else {
     houseScore.innerText = "Down to the dungeons in slytherin";
   }
