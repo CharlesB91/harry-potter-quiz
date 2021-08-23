27febb47f8cd9a8c4fc13b80a5f95edea5624026
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-08-23 15:33:13 +0100

    js code added to display house badge depending on score

--- assets/js/end.js
+++ assets/js/end.js
@@ -4,9 +4,9 @@ let mostRecentScore = localStorage.getItem("most-recent-score");
 let scoreAlert = document.getElementById("sorting-hat-greeting");
 let houseScore = document.getElementById("house-sort");
 let gryff = document.getElementById("gryffindor");
-// let huffle = document.getElementById("huggle-puff");
-// let raven = document.getElementById("ravenclaw");
-// let slyth = document.getElementById("slytherin");
+let huffle = document.getElementById("hufflepuff");
+let raven = document.getElementById("ravenclaw");
+let slyth = document.getElementById("slytherin");
 scoresOnDoors();
 
 // Sorting Function. Depending on
@@ -20,16 +20,13 @@ function scoresOnDoors() {
     gryff.classList.remove("hide2");
     houseScore.innerText = "Better Be gryffindor";
   } else if (mostRecentScore < 5 && mostRecentScore > 3) {
-    huffle.classList.remove("huggle-puff");
+    huffle.classList.remove("hide3");
     houseScore.innerText = "Welcome To Hufflepuff";
   } else if (mostRecentScore < 4 && mostRecentScore > 2) {
-    raven.classList.remove("ravenclaw");
-    houseScore.innerText = "Welcome To ravenclaw";
-  } else if (mostRecentScore < 3 && mostRecentScore > 1) {
-    slyth.classList.remove("slytherin");
+    raven.classList.remove("hide4");
     houseScore.innerText = "Welcome To ravenclaw";
   } else {
-    slyth.classList.remove("slytherin");
+    slyth.classList.remove("hide5");
     houseScore.innerText = "Down to the dungeons in slytherin";
   }
 }
