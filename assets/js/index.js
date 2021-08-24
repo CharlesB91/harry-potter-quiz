78b8bdd19678bae6d269852d8f17b60db8d5ffdc
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-08-24 17:29:34 +0100

    modal screen added for instructions

--- assets/js/index.js
+++ assets/js/index.js
@@ -95,7 +95,14 @@ let quizquestions = [
 
 // Global Selectors - DOM //
 
-let nav = document.getElementById("myNav");
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
 let questionSelect = document.getElementById("question-placeholder");
 let answers = Array.from(document.getElementsByClassName("choice-text"));
 let homeScreen = document.getElementById("home");
@@ -109,6 +116,22 @@ let counterText = document.getElementById("questionCounter");
 let currentQuestion = {};
 let avaliableQuestions = [];
 let questionCounter = 0;
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
 let startClick = document.getElementById("start-btn");
 startClick.addEventListener("click", start);
