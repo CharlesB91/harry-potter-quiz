c77ca8966ed2a9ab6072fbbb5c5ffe67a9270aa9
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-08-29 12:42:53 +0100

    nav added to end html with js end file updated

--- assets/js/end.js
+++ assets/js/end.js
@@ -37,3 +37,25 @@ function scoresOnDoors() {
 }
 
 localStorage.clear();
+
+// Instruction Box //
+
+let modal = document.getElementById("myModal");
+
+let btn = document.getElementById("instruct");
+
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
